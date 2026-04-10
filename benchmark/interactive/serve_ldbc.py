0c7b6e616a5095763b389ef9d7c317ff8f6fa006
nengli.ln <nengli.ln@alibaba-inc.com>
2026-04-10 16:11:08 +0800

    update server config

--- benchmark/interactive/serve_ldbc.py
+++ benchmark/interactive/serve_ldbc.py
@@ -5,7 +5,7 @@ import neug
 DB_DIR = os.path.join(os.getcwd(), "ldbc")
 
 if __name__ == "__main__":
-    db = neug.Database(DB_DIR)
+    db = neug.Database(DB_DIR, buffer_strategy="M_HUGE", max_thread_num=192)
     print(f"Database opened at {DB_DIR}")
     uri = db.serve(port=57380, host="0.0.0.0", blocking=True)
     print(f"Serving at {uri}")
