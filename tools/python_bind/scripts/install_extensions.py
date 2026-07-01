6328cc1aeeff14fb52a0fdf1134b6df6347e09ea
nengli.ln <nengli.ln@alibaba-inc.com>
2026-07-01 17:56:37 +0800

    fix extension workflow

---
+++ tools/python_bind/scripts/install_extensions.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Alibaba Group Holding Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Install NeuG extensions (httpfs, parquet, gds) from OSS.
+
+Used by nightly-extension-test.yml to download pre-built extension
+binaries before running extension tests.  This is necessary because
+the neug PyPI wheel does not ship compiled extension binaries.
+"""
+
+import logging
+import sys
+
+from neug import Connection, Database
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
+
+EXTENSIONS = ["httpfs", "parquet", "gds"]
+
+
+def install_extensions():
+    db = Database(":memory:")
+    conn = Connection(db)
+    for ext in EXTENSIONS:
+        logger.info("Installing extension: %s", ext)
+        conn.execute(f"INSTALL EXTENSION {ext}")
+        logger.info("Extension %s installed successfully", ext)
+    logger.info("All extensions installed successfully")
+
+
+if __name__ == "__main__":
+    try:
+        install_extensions()
+    except Exception as exc:
+        logger.error("Failed to install extensions: %s", exc)
+        sys.exit(1)
